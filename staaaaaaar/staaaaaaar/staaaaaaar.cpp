6ede81b986f34837f011b36ebdbb80aa78ea725b
wwwhite111 <144310770+wwwhite111@users.noreply.github.com>
2023-09-07 15:41:28 +0900

    staaaaaaar

--- staaaaaaar/staaaaaaar/staaaaaaar.cpp
+++ staaaaaaar/staaaaaaar/staaaaaaar.cpp
@@ -2,6 +2,23 @@
 
 int main()
 {
-	printf("hi");
+	int a;
+
+	scanf_s("%d", &a);
+
+	for (int i = 0; i < a; i++)
+	{
+
+		for (int b = i; b <= (a - 1); b++)
+		{
+			printf(" ");
+		}
+
+		for (int j = 0; j <= (i * 2); j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 	return 0;
 }
