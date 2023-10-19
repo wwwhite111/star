98cc35842a37592f053a4a391e754d5befe98e9d
wwwhite111 <144310770+wwwhite111@users.noreply.github.com>
2023-10-19 16:01:01 +0900

    ep_02

--- Project_1019/Project_1019/ep_01.cpp
+++ Project_1019/Project_1019/ep_01.cpp
@@ -1,42 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-struct Student
-{
-	int sno;
-	char name[10];
-	int score;
-};
-
-int main()
-{
-	int a;
-	int sum = 0;
-	
-	printf("학생수 입력 : ");
-	scanf_s("%d", &a);
-
-	/*int* scores;
-	scores = (int*)malloc(sizeof(int) * a);*/
-
-	struct Student* std;
-	std = (struct Student*)malloc(sizeof(struct Student) * a);
-
-	for (int i = 0; i < a; i++)
-	{
-		printf("학생 # %d-%d성적 입력 : ", a, i + 1);
-		scanf("%d", &(std[i].sno));
-
-		printf("학생 # %d-%d이름 입력 : ", a, i + 1);
-		scanf("%s", &std[i].name, 10);
-
-		printf("학생 # %d-%d점수 입력 : ", a, i + 1);
-		scanf("%d", &(std[i].score));
-	}
-	/*for (int i = 0; i < a; i++)
-	{
-		sum += scores[i];
-	}*/
-
-	/*printf("총점 : %d", sum);*/
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//struct Student
+//{
+//	int sno;
+//	char name[10];
+//	int score;
+//};
+//
+//int main()
+//{
+//	int a;
+//	int sum = 0;
+//	
+//	printf("학생수 입력 : ");
+//	scanf_s("%d", &a);
+//
+//	/*int* scores;
+//	scores = (int*)malloc(sizeof(int) * a);*/
+//
+//	struct Student* std;
+//	std = (struct Student*)malloc(sizeof(struct Student) * a);
+//
+//	for (int i = 0; i < a; i++)
+//	{
+//		printf("학생 # %d-%d성적 입력 : ", a, i + 1);
+//		scanf("%d", &(std[i].sno));
+//
+//		printf("학생 # %d-%d이름 입력 : ", a, i + 1);
+//		scanf("%s", &std[i].name, 10);
+//
+//		printf("학생 # %d-%d점수 입력 : ", a, i + 1);
+//		scanf("%d", &(std[i].score));
+//	}
+//	/*for (int i = 0; i < a; i++)
+//	{
+//		sum += scores[i];
+//	}*/
+//
+//	/*printf("총점 : %d", sum);*/
+//}
--- Project_1019/Project_1019/ep_02.cpp
+++ Project_1019/Project_1019/ep_02.cpp
@@ -1,6 +1,105 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+typedef struct _node {
+	int data;
+	struct _node* next;
+} node;
+
+void print_list(node* head)
+{
+	node* p = head;
+
+	while (p->next != NULL)
+	{
+		p = p->next;
+		printf("%d ", p->data);
+	}
+	printf("\n");
+}
+
+void insort_node_last(node* head, int data)
+{
+	node* new_node = (node*)malloc(sizeof(node));
+	new_node->data = data;
+
+	node* p = head;
+	while (p->next != NULL)
+	{
+		p = p->next;
+	}
+	p->next = new_node;
+	new_node->next = NULL;
+}
+
+void insort_node_first(node* head, int data)
+{
+	node* new_node = (node*)malloc(sizeof(node));
+	new_node->data = data;
+
+	new_node->next = head->next;
+	head->next = new_node;
+}
+
+void del_node_first(node* head)
+{
+	node* p = head->next->next;
+	free(head->next);
+	head->next = p;
+}
+
+void del_node_last(node* head)
+{
+	node* p_prev;
+	node* p = head;
+	while (p->next != NULL)
+	{
+		p = p->next;
+	}
+	free(p);
+
+	p_prev->next = NULL;
+}
 
 int main()
 {
+	node* head;
+	head = (node*)malloc(sizeof(node));
+	head->next = NULL;
 
+	node* n1 = (node*)malloc(sizeof(node));
+	n1->data = 1;
+	n1->next = NULL;
+	head->next = n1;
+
+	node* n2 = (node*)malloc(sizeof(node));
+	n2->data = 2;
+	n2->next = NULL;
+	n1->next = n2;
+
+	node* n3 = (node*)malloc(sizeof(node));
+	n3->data = 3;
+	n3->next = NULL;
+	n2->next = n3;
+
+	print_list(head);
+
+	insort_node_last(head, 4);
+	insort_node_last(head, 5);
+
+	print_list(head);
+
+	insort_node_first(head, 6);
+
+	print_list(head);
+
+	del_node_first(head);
+
+	print_list(head);
+
+	del_node_last(head);
+
+	print_list(head);
+	
+	return 0;
 }
